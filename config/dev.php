dfc8cab89af131f6305b02ccf8f668c01cce267c
Juillet Nicolas <juillet.n@hotmail.fr>
2020-11-27 19:19:32 +0100

    bases site papacionu

---
+++ config/dev.php
@@ -0,0 +1,9 @@
+<?php
+
+// Nos constantes
+const HOST = 'localhost';
+const DB_NAME = 'papacionu';
+const CHARSET = 'utf8';
+const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
+const DB_USER = 'dbu461722';
+const DB_PASS = 'nicoAsc159vhu753kom0@';
